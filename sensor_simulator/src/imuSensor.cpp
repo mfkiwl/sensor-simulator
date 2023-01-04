56e9b3a84c79a1edf67737bf68bba31a5161fca0
Parker Barrett <parkerb7@vt.edu>
2023-01-04 14:14:28 -0500

    Outline structure and basic looping algorithm for generating IMU measurements

--- sensor_simulator/src/imuSensor.cpp
+++ sensor_simulator/src/imuSensor.cpp
@@ -20,15 +20,15 @@ bool imuSensor::init(imuSensorSimData_t imuConfig) {
     // Set Class Variables
     rate_ = imuConfig.rate;
     accel_bias_repeatability_rms_mg_ = imuConfig.accel_bias_repeatability;
-	accel_bias_instability_rms_mg_ = imuConfig.accel_bias_instability;
-	accel_sf_sigma_ppm_ = imuConfig.accel_sf;
-	accel_mis_sigma_urad_ = imuConfig.accel_mis;
-	accel_vrw_m_s_sqrtHr_ = imuConfig.accel_vrw;
-	gyro_bias_repeatability_rms_deg_hr_ = imuConfig.gyro_bias_repeatability;
-	gyro_bias_instability_rms_deg_hr_ = imuConfig.gyro_bias_instability;
-	gyro_sf_sigma_ppm_ = imuConfig.gyro_sf;
-	gyro_mis_sigma_urad_ = imuConfig.gyro_mis;
-	gyro_arw_deg_sqrtHr_ = imuConfig.gyro_arw;
+    accel_bias_instability_rms_mg_ = imuConfig.accel_bias_instability;
+    accel_sf_sigma_ppm_ = imuConfig.accel_sf;
+    accel_mis_sigma_urad_ = imuConfig.accel_mis;
+    accel_vrw_m_s_sqrtHr_ = imuConfig.accel_vrw;
+    gyro_bias_repeatability_rms_deg_hr_ = imuConfig.gyro_bias_repeatability;
+    gyro_bias_instability_rms_deg_hr_ = imuConfig.gyro_bias_instability;
+    gyro_sf_sigma_ppm_ = imuConfig.gyro_sf;
+    gyro_mis_sigma_urad_ = imuConfig.gyro_mis;
+    gyro_arw_deg_sqrtHr_ = imuConfig.gyro_arw;
 
     // Successful Return
     return true;
@@ -37,8 +37,8 @@ bool imuSensor::init(imuSensorSimData_t imuConfig) {
 
 // Run Generate IMU Sensor Measurements
 bool imuSensor::generateImuMeasurements(nedTrajSensorSimData_t nedTraj,
-			                            std::queue<int64_t> &imuTov,
-				                        std::queue<Eigen::Vector3d> &dvData,
+			                std::queue<int64_t> &imuTov,
+				        std::queue<Eigen::Vector3d> &dvData,
                                         std::queue<Eigen::Vector3d> &dThData) {
     
     // Random Number Generators
@@ -52,10 +52,49 @@ bool imuSensor::generateImuMeasurements(nedTrajSensorSimData_t nedTraj,
     std::normal_distribution<double> sigGyroSf(0.0, gyro_sf_sigma_ppm_);
     std::normal_distribution<double> sigGyroMis(0.0, gyro_mis_sigma_urad_);
 
-
-
+    // Initialization
+    int64_t tCurr = nedTraj.tov[0];
+    int64_t dt = (int64_t) ((1.0 / rate_) * 1.0e6);
+    int idxLower = 0;
+    int idxUpper = 1;
 
     // Main Loop to Generate IMU Measurements
+    while (tCurr < nedTraj.tov[nedTraj.tov.size()-1]) {
+        
+	// Find Indices of Truth Trajectory around Current Time
+	while (tCurr >= nedTraj.tov[idxUpper]) {
+	    idxLower++;
+	    idxUpper++;
+	}
+
+	// Get Upper and Lower Index Position/Velocity/Attitude
+	Eigen::Vector3d lowerLla = nedTraj.lla[idxLower];
+	Eigen::Vector3d upperLla = nedTraj.lla[idxUpper];
+	Eigen::Vector3d lowerVelNed = nedTraj.vNed[idxLower];
+	Eigen::Vector3d upperVelNed = nedTraj.vNed[idxUpper];
+	Eigen::Vector3d lowerRph = nedTraj.rph[idxLower];
+	Eigen::Vector3d upperRph = nedTraj.rph[idxUpper];
+
+        // Compute Rotations
+
+	// Rotate PVA from NED to J2K Inertial Frame
+
+        // Interpolate Inertial Frame Data
+	
+	// Compute Gravity
+	
+	// Compute Truth Delta Velocity and Delta Thetas
+	
+	// Propagate IMU Errors Forward
+
+	// Compute Delta Velocity and Delta Theta Noisy Measurements
+
+	// Push Measurements to Output Queues
+
+	// Update Current Time
+	tCurr += dt;
+
+    }
 
     // Successful Return
     std::cout << "[imuSensor::generateImuMeasurements] Completed IMU Measurement Generation" << std::endl;
