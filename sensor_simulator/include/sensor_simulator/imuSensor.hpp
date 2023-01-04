98b5f655095721e452755061d3194cd64a28dd38
Parker Barrett <parkerb7@vt.edu>
2023-01-04 06:36:25 -0500

    Setup IMU sensor simulator class

--- sensor_simulator/include/sensor_simulator/imuSensor.hpp
+++ sensor_simulator/include/sensor_simulator/imuSensor.hpp
@@ -10,7 +10,10 @@
 // Include Headers
 #pragma once
 #include <iostream>
+#include <queue>
+#include <random>
 #include <Eigen/Dense>
+#include <Rotations.hpp>
 #include <dataTypes.hpp>
 
 // IMU Sensor Model Class
@@ -19,20 +22,6 @@ class imuSensor {
     // Public Class Members/Functions
     public:
         
-        /* @generateImuMeasurements
-            Inputs:
-                imuConfig: imuSensorSimData_t struct containing IMU model parameters
-            Outputs:
-
-            Description:
-                Function which takes in the IMU model parameters and trajectory and generates
-                a time history of measurements.
-        */
-        bool generateImuMeasurements(imuSensorSimData_t imuConfig);
-
-    // Private Class Members/Function
-    private:
-
         /* @init
             Inputs:
                 imuConfig: imuSensorSimData_t struct containing IMU model parameters
@@ -41,9 +30,37 @@ class imuSensor {
                 Function which initializes the IMU sensor model.
         */
         bool init(imuSensorSimData_t imuConfig);
-
-        // IMU Sensor Sim Data
-        imuSensorSimData_t imuConfig_;
         
+        /* @generateImuMeasurements
+            Inputs:
+	            nedTraj: nedTrajSensorSimData_t containing NED truth trajectory
+            Outputs:
+                imuTov: std::queue<int64_t> FIFO queue containing IMU measurement UTC timestamps
+                dVData: std::queue<Eigen::Vector3d> FIFO queue containing IMU delta velocity measurements
+                dThData: std::queue<Eigen::Vector3d> FIFO queue containing IMU delta theta measurements
+        */
+        bool generateImuMeasurements(nedTrajSensorSimData_t nedTraj,
+			                         std::queue<int64_t> &imuTov,
+				                     std::queue<Eigen::Vector3d> &dvData,
+                                     std::queue<Eigen::Vector3d> &dThData);
 
+    // Private Class Members/Function
+    private:
+
+        // IMU Sensor Sim Model Parameters
+        double rate_;
+	    double accel_bias_repeatability_rms_mg_;
+		double accel_bias_instability_rms_mg_;
+		double accel_sf_sigma_ppm_;
+		double accel_mis_sigma_urad_;
+		double accel_vrw_m_s_sqrtHr_;
+		double gyro_bias_repeatability_rms_deg_hr_;
+		double gyro_bias_instability_rms_deg_hr_;
+		double gyro_sf_sigma_ppm_;
+		double gyro_mis_sigma_urad_;
+		double gyro_arw_deg_sqrtHr_;
+
+	    // NavUtils Rotations
+	    Rotations rot_;
+        
 };
--- sensor_simulator/src/imuSensor.cpp
+++ sensor_simulator/src/imuSensor.cpp
@@ -17,7 +17,18 @@
 // Initialize IMU Model
 bool imuSensor::init(imuSensorSimData_t imuConfig) {
 
-    //
+    // Set Class Variables
+    rate_ = imuConfig.rate;
+    accel_bias_repeatability_rms_mg_ = imuConfig.accel_bias_repeatability;
+	accel_bias_instability_rms_mg_ = imuConfig.accel_bias_instability;
+	accel_sf_sigma_ppm_ = imuConfig.accel_sf;
+	accel_mis_sigma_urad_ = imuConfig.accel_mis;
+	accel_vrw_m_s_sqrtHr_ = imuConfig.accel_vrw;
+	gyro_bias_repeatability_rms_deg_hr_ = imuConfig.gyro_bias_repeatability;
+	gyro_bias_instability_rms_deg_hr_ = imuConfig.gyro_bias_instability;
+	gyro_sf_sigma_ppm_ = imuConfig.gyro_sf;
+	gyro_mis_sigma_urad_ = imuConfig.gyro_mis;
+	gyro_arw_deg_sqrtHr_ = imuConfig.gyro_arw;
 
     // Successful Return
     return true;
@@ -25,13 +36,24 @@ bool imuSensor::init(imuSensorSimData_t imuConfig) {
 }
 
 // Run Generate IMU Sensor Measurements
-bool imuSensor::generateImuMeasurements(imuSensorSimData_t imuConfig) {
+bool imuSensor::generateImuMeasurements(nedTrajSensorSimData_t nedTraj,
+			                            std::queue<int64_t> &imuTov,
+				                        std::queue<Eigen::Vector3d> &dvData,
+                                        std::queue<Eigen::Vector3d> &dThData) {
     
-    // Initialize IMU Thread
-    if (!init(imuConfig)) {
-        std::cout << "[imuSensor::generateImuMeasurements] Failed to initialize IMU sensor model" << std::endl;
-        return false;
-    }
+    // Random Number Generators
+    std::default_random_engine generator;
+    std::normal_distribution<double> sigAccelBiasRepeatability(0.0, accel_bias_repeatability_rms_mg_);
+    std::normal_distribution<double> sigAccelBiasInstability(0.0, accel_bias_instability_rms_mg_);
+    std::normal_distribution<double> sigAccelSf(0.0, accel_sf_sigma_ppm_);
+    std::normal_distribution<double> sigAccelMis(0.0, accel_mis_sigma_urad_);
+    std::normal_distribution<double> sigGyroBiasRepeatability(0.0, gyro_bias_repeatability_rms_deg_hr_);
+    std::normal_distribution<double> sigGyroBiasInstability(0.0, gyro_bias_instability_rms_deg_hr_);
+    std::normal_distribution<double> sigGyroSf(0.0, gyro_sf_sigma_ppm_);
+    std::normal_distribution<double> sigGyroMis(0.0, gyro_mis_sigma_urad_);
+
+
+
 
     // Main Loop to Generate IMU Measurements
 
